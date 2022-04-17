6617d938a1169fd7872772f50a3a8b5f0dde55a4
igdev <minhhunghuynh1106@gmail.com>
2022-04-17 19:06:52 +0700

    chore: add styles

--- src/lib/tailwind/animation.js
+++ src/lib/tailwind/animation.js
@@ -1,4 +1,7 @@
 module.exports = {
   spinner: 'spinner 1.4s ease infinite',
   zoomIn: 'zoomIn 250ms ease',
+  skeleton: 'skeleton 1s linear reverse infinite forwards',
+  'like-button': 'like-button 450ms ease-in-out',
+  'like-feed': 'like-feed 1.250s ease-in-out forwards',
 };
--- src/lib/tailwind/colors.js
+++ src/lib/tailwind/colors.js
@@ -8,6 +8,4 @@ module.exports = {
     red: '#db4437',
   },
   modal: 'rgb(0 0 0 / 0.85)',
-  instagram:
-    'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)',
 };
