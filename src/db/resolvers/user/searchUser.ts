08a9b3f28a7c8d5dd2d4beadae5e12df007cd659
igdev <minhhunghuynh1106@gmail.com>
2022-04-27 22:45:00 +0700

    fix: missing middleware

--- src/db/resolvers/user/searchUser.ts
+++ src/db/resolvers/user/searchUser.ts
@@ -1,15 +1,17 @@
-import { Arg, ClassType, Int, Mutation, Resolver } from 'type-graphql';
+import { Arg, ClassType, Int, Mutation, Resolver, UseMiddleware } from 'type-graphql';
 import { User } from '~/db/models';
 
 // types
 import { SearchUserResponse } from '~/db/types/responses';
 
+import { verifyAuth } from '~/db/middlewares';
 import respond from '~/helpers/respond';
 
 const searchUser = (Base: ClassType) => {
   @Resolver()
   class SearchUser extends Base {
     @Mutation((_returns) => SearchUserResponse)
+    @UseMiddleware(verifyAuth)
     searchUser(
       @Arg('query') query: string,
       @Arg('limit', (_type) => Int) limit: number,
