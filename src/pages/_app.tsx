7f21e7c02d81f266ecb845ee6c2e597ea1d181af
igdev <minhhunghuynh1106@gmail.com>
2022-05-07 22:21:53 +0700

    feat: show progress bar when navigating

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
 
 import { ApolloProvider } from '@apollo/client';
 import { config } from '@fortawesome/fontawesome-svg-core';
+import ProgressBar from '@badrap/bar-of-progress';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
 import { useApollo } from '~/lib/apolloClient';
@@ -14,6 +16,15 @@ import SocketProvider from '~/contexts/SocketContext';
 import '../styles/index.scss';
 
 config.autoAddCss = false;
+
+const progressBar = new ProgressBar({
+  size: 4,
+  className: 'bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400',
+});
+
+Router.events.on('routeChangeStart', progressBar.start);
+Router.events.on('routeChangeComplete', progressBar.finish);
+Router.events.on('routeChangeError', progressBar.finish);
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps);
