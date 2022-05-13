e3d6f4d4f2255e4515fab66d5dd270b30ec3cc4c
igdev <minhhunghuynh1106@gmail.com>
2022-05-13 10:44:59 +0700

    refactor: pass ref

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,4 +1,4 @@
-import { ImgHTMLAttributes, useEffect, useRef, useState } from 'react';
+import { forwardRef, ImgHTMLAttributes, useEffect, useRef, useState } from 'react';
 
 import clsx from 'clsx';
 
@@ -8,59 +8,64 @@ interface SkeletonProps extends ImgHTMLAttributes<HTMLImageElement> {
   objectFit?: 'contain' | 'cover';
 }
 
-const Skeleton = ({ alt, src, rounded, className, objectFit, online, ...rest }: SkeletonProps) => {
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+const Skeleton = forwardRef<any, SkeletonProps>(
+  ({ alt, src, rounded, className, objectFit, online, ...rest }, forwardRef) => {
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
-  const imageRef = useRef<HTMLImageElement>(null);
+    const imageRef = useRef<HTMLImageElement>(null);
 
-  useEffect(() => {
-    if (imageRef.current?.complete) setIsLoaded(true);
-  }, []);
+    useEffect(() => {
+      if (imageRef.current?.complete) setIsLoaded(true);
+    }, []);
 
-  return (
-    <div
-      className={clsx(
-        'relative',
-        'flex-shrink-0',
-        className,
-        objectFit === 'cover' && 'w-full h-full',
-      )}
-    >
-      <img
-        {...rest}
-        ref={imageRef}
-        src={src}
-        alt={alt}
+    return (
+      <div
+        ref={forwardRef}
         className={clsx(
-          'w-full',
-          isLoaded ? 'opacity-100 visible' : 'opacity-0 invisible',
-          rounded && 'rounded-full',
-          objectFit && ['h-full', objectFit === 'cover' ? 'object-cover' : 'object-contain'],
+          'relative',
+          'flex-shrink-0',
+          className,
+          objectFit === 'cover' && 'w-full h-full',
         )}
-        onLoad={() => setIsLoaded(true)}
-        draggable={false}
-      />
-      {!isLoaded && (
-        <div
+      >
+        <img
+          {...rest}
+          ref={imageRef}
+          src={src}
+          alt={alt}
           className={clsx(
-            'absolute inset-0',
-            'bg-[length:200%] bg-skeleton',
-            'animate-skeleton',
+            'w-full',
+            isLoaded ? 'opacity-100 visible' : 'opacity-0 invisible',
             rounded && 'rounded-full',
+            objectFit && ['h-full', objectFit === 'cover' ? 'object-cover' : 'object-contain'],
           )}
+          onLoad={() => setIsLoaded(true)}
+          draggable={false}
         />
-      )}
-      {online && (
-        <div
-          className={clsx(
-            'absolute -bottom-0.5 -right-0.5',
-            'w-4 h-4 border-3 border-white rounded-full',
-            'bg-base-green',
-          )}
-        />
-      )}
-    </div>
-  );
-};
+        {!isLoaded && (
+          <div
+            className={clsx(
+              'absolute inset-0',
+              'bg-[length:200%] bg-skeleton',
+              'animate-skeleton',
+              rounded && 'rounded-full',
+            )}
+          />
+        )}
+        {online && (
+          <div
+            className={clsx(
+              'absolute -bottom-0.5 -right-0.5',
+              'w-4 h-4 border-3 border-white rounded-full',
+              'bg-base-green',
+            )}
+          />
+        )}
+      </div>
+    );
+  },
+);
+
+Skeleton.displayName = 'Skeleton';
 
 export default Skeleton;
