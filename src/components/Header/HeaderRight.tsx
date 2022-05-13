b1009df8388886476402b80e16358e90ac2e29ad
igdev <minhhunghuynh1106@gmail.com>
2022-05-13 10:01:22 +0700

    feat: active route

--- src/components/Header/HeaderRight.tsx
+++ src/components/Header/HeaderRight.tsx
@@ -12,7 +12,7 @@ import IconHome from '../Icon/IconHome';
 import IconMessenger from '../Icon/IconMessenger';
 
 const HeaderRight = () => {
-  const { showModal } = useModalContext();
+  const { modalTypes, showModal } = useModalContext();
 
   const router = useRouter();
 
@@ -21,17 +21,17 @@ const HeaderRight = () => {
       <IconHome
         onClick={() => router.push(ROUTES.HOME)}
         className={clsx('cursor-pointer')}
-        active
+        active={router.pathname === ROUTES.HOME}
       />
       <IconMessenger
         onClick={() => router.replace(ROUTES.INBOX, undefined, { shallow: true })}
         className={clsx('cursor-pointer')}
-        active={false}
+        active={router.pathname === ROUTES.INBOX}
       />
       <IconCreate
         onClick={() => showModal(MODAL_TYPES.POST_CREATOR)}
         className={clsx('cursor-pointer')}
-        active={false}
+        active={modalTypes.includes(MODAL_TYPES.POST_CREATOR)}
       />
       <IconCompass className={clsx('cursor-pointer')} active={false} />
       <IconHeart className={clsx('cursor-pointer')} active={false} />
