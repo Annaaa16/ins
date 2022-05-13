19d940fa09025f9f81dd0456718e8cbc95d10fcf
igdev <minhhunghuynh1106@gmail.com>
2022-05-13 10:01:41 +0700

    styles: add leading

--- src/components/Header/HeaderMiddleSearch.tsx
+++ src/components/Header/HeaderMiddleSearch.tsx
@@ -22,7 +22,7 @@ const HeaderMiddleSearch = ({ users }: HeaderMiddleSearchProps) => {
       <div className={clsx('flex-center flex-col text-center w-full h-full px-10', 'text-gray')}>
         <IconNotFound className='w-2/6' />
         <h3 className='font-bold text-base mt-3'>We didn&apos;t find any results</h3>
-        <p className={clsx('text-sm mt-1', 'text-base-gray')}>
+        <p className={clsx('text-sm mt-1 leading-5', 'text-base-gray')}>
           Make sure everything is spelled correctly or try different keywords.
         </p>
       </div>
