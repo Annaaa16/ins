d5bac660dab79b1b924a567ac4541fa34bbe5816
igdev <minhhunghuynh1106@gmail.com>
2022-05-19 17:26:20 +0700

    feat: handle image error

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, ImgHTMLAttributes, useEffect, useRef, useState } from 'react';
+import { forwardRef, ImgHTMLAttributes, memo, ReactNode, useEffect, useRef, useState } from 'react';
 
 import clsx from 'clsx';
 
@@ -6,17 +6,83 @@ interface SkeletonProps extends ImgHTMLAttributes<HTMLImageElement> {
   rounded?: boolean;
   online?: boolean;
   objectFit?: 'contain' | 'cover';
+  placeholderError?: ReactNode | string;
 }
 
+const isImageLoaded = (src: string) => {
+  return new Promise<boolean>((resolve) => {
+    const img = document.createElement('img');
+
+    img.onerror = () => resolve(false);
+    img.onload = () => resolve(true);
+    img.src = src;
+  });
+};
+
 const Skeleton = forwardRef<any, SkeletonProps>(
-  ({ alt, src, rounded, className, objectFit, online, ...rest }, forwardRef) => {
+  ({ alt, src, rounded, className, objectFit, online, placeholderError, ...rest }, forwardRef) => {
     const [isLoaded, setIsLoaded] = useState<boolean>(false);
+    const [isError, setIsError] = useState<boolean>(false);
 
     const imageRef = useRef<HTMLImageElement>(null);
 
     useEffect(() => {
-      if (imageRef.current?.complete) setIsLoaded(true);
-    }, []);
+      const image = imageRef.current;
+
+      if (image == null || src == null) return;
+
+      if (image.complete) setIsLoaded(true);
+
+      isImageLoaded(src).then((isSuccess) => {
+        if (!isSuccess) setIsError(true);
+      });
+    }, [src]);
+
+    let body = null;
+
+    if (isError && typeof placeholderError === 'function') {
+      const ErrorComponent = placeholderError;
+
+      body = <ErrorComponent />;
+    } else {
+      body = (
+        <>
+          <img
+            {...rest}
+            ref={imageRef}
+            src={src}
+            alt={alt}
+            className={clsx(
+              'w-full overflow-hidden',
+              isLoaded ? 'opacity-100 visible' : 'opacity-0 invisible',
+              rounded && 'rounded-full',
+              objectFit && ['h-full', objectFit === 'cover' ? 'object-cover' : 'object-contain'],
+            )}
+            onLoad={() => setIsLoaded(true)}
+            draggable={false}
+          />
+          {!isLoaded && !isError && (
+            <div
+              className={clsx(
+                'absolute inset-0',
+                'bg-[length:200%] bg-skeleton',
+                'animate-skeleton',
+                rounded && 'rounded-full',
+              )}
+            />
+          )}
+          {online && (
+            <div
+              className={clsx(
+                'absolute -bottom-0.5 -right-0.5',
+                'w-4 h-4 border-3 border-white rounded-full',
+                'bg-base-green',
+              )}
+            />
+          )}
+        </>
+      );
+    }
 
     return (
       <div
@@ -28,39 +94,7 @@ const Skeleton = forwardRef<any, SkeletonProps>(
           objectFit === 'cover' && 'w-full h-full',
         )}
       >
-        <img
-          {...rest}
-          ref={imageRef}
-          src={src}
-          alt={alt}
-          className={clsx(
-            'w-full',
-            isLoaded ? 'opacity-100 visible' : 'opacity-0 invisible',
-            rounded && 'rounded-full',
-            objectFit && ['h-full', objectFit === 'cover' ? 'object-cover' : 'object-contain'],
-          )}
-          onLoad={() => setIsLoaded(true)}
-          draggable={false}
-        />
-        {!isLoaded && (
-          <div
-            className={clsx(
-              'absolute inset-0',
-              'bg-[length:200%] bg-skeleton',
-              'animate-skeleton',
-              rounded && 'rounded-full',
-            )}
-          />
-        )}
-        {online && (
-          <div
-            className={clsx(
-              'absolute -bottom-0.5 -right-0.5',
-              'w-4 h-4 border-3 border-white rounded-full',
-              'bg-base-green',
-            )}
-          />
-        )}
+        {body}
       </div>
     );
   },
@@ -68,4 +102,4 @@ const Skeleton = forwardRef<any, SkeletonProps>(
 
 Skeleton.displayName = 'Skeleton';
 
-export default Skeleton;
+export default memo(Skeleton);
